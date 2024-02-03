cd210d67ab57760b532fd99789d91c302d7d572e
ShaharAshe <shaharas30@gmail.com>
2024-02-03 20:32:54 +0200

    date required

--- js/index.js
+++ js/index.js
@@ -6,11 +6,14 @@ const utilities = (function() {
     const date_type_ev =  document.getElementById("date_type");
     const earth_date_ev =  document.getElementById("earth_date");
     const sol_number_ev =  document.getElementById("sol_number");
-    const date_of_cam_ev = document.querySelectorAll(".date_of_cam");
+    const date_of_cam_none_ev = document.querySelectorAll(".date_of_cam_none");
+    const date_of_cam_alert_ev = document.querySelectorAll(".date_of_cam_alert");
     const rover_ev =  document.getElementById("rover");
     const camera_ev =  document.getElementById("camera");
     const search_ev = document.querySelector(".search_form_b");
-    const reset_ev = document.querySelector(".reset_form_b")
+    const reset_ev = document.querySelector(".reset_form_b");
+    const pic_result_ev = document.querySelector(".pic_result");
+    const bad_val_gu_ev = document.querySelector(".bad_val_gu");
 
     const spinner = document.querySelector(".spinner-border");
     return {
@@ -18,12 +21,15 @@ const utilities = (function() {
         date_type_ev: date_type_ev,
         earth_date_ev: earth_date_ev,
         sol_number_ev: sol_number_ev,
-        date_of_cam_ev: date_of_cam_ev,
+        date_of_cam_none_ev: date_of_cam_none_ev,
+        date_of_cam_alert_ev: date_of_cam_alert_ev,
         rover_ev: rover_ev,
         camera_ev: camera_ev,
         spinner: spinner,
         search_ev: search_ev,
         reset_ev: reset_ev,
+        pic_result_ev: pic_result_ev,
+        bad_val_gu_ev: bad_val_gu_ev,
     };
 })()
 
@@ -82,41 +88,60 @@ const funcs = (function (){
                 utilities.earth_date_ev.setAttribute("min", utilities.api_data[utilities.rover_ev.value - 1]["landing_date"]);
             }
         },
+        reset_form: function (){
+            utilities.date_type_ev.selectedIndex = 0;
+            utilities.rover_ev.selectedIndex = 0;
+            utilities.date_of_cam_none_ev.forEach(date => {
+                date.classList.add('d-none');
+            });
+            utilities.earth_date_ev.value = "";
+            utilities.sol_number_ev.value = "";
+
+            while(utilities.camera_ev.childElementCount !== 1) {
+                utilities.camera_ev.removeChild(utilities.camera_ev.lastChild);
+            }
+        },
+        set_date_type: function (){
+            utilities.date_of_cam_none_ev.forEach(date=>{
+                date.classList.add('d-none');
+            });
+            if(utilities.date_type_ev.value !== '0')
+                utilities.date_of_cam_none_ev[Number(utilities.date_type_ev.value)-1].classList.remove('d-none')
+        },
     }
 })()
 
 const main = (() => {
     return {
         main_func: () => {
-            page_data.load_data()
+            page_data.load_data();
             // we should display the error to the user
 
             utilities.date_type_ev.addEventListener("mouseup", function (){
-                utilities.date_of_cam_ev.forEach(date=>{
-                    date.classList.add('d-none');
-                });
-                if(utilities.date_type_ev.value !== '0')
-                    utilities.date_of_cam_ev[Number(utilities.date_type_ev.value)-1].classList.remove('d-none')
-            })
+                funcs.set_date_type();
+            });
 
             utilities.rover_ev.addEventListener("mouseup", function (){
                 funcs.load_cameras();
                 funcs.update_date();
-            })
+            });
 
             utilities.reset_ev.addEventListener("click", function () {
-                utilities.date_type_ev.selectedIndex = 0;
-                utilities.rover_ev.selectedIndex = 0;
-                utilities.date_of_cam_ev.forEach(date => {
-                    date.classList.add('d-none');
-                });
-                utilities.earth_date_ev.value = "";
-                utilities.sol_number_ev.value = "";
+                funcs.reset_form();
+            });
 
-                while(utilities.camera_ev.childElementCount !== 1) {
-                    utilities.camera_ev.removeChild(utilities.camera_ev.lastChild);
-                }
-            })
+            utilities.search_ev.addEventListener("click", function (){
+                let alert_msg = ""
+                alert_msg += ""
+                utilities.bad_val_gu_ev.classList.add("d-none")
+                if (utilities.date_type_ev.value === "0")
+                    alert_msg += "The date type must not be empty";
+                else if (!utilities.date_of_cam_alert_ev[Number(utilities.date_type_ev.value)-1].value)
+                    alert_msg += "The date must not be empty";
+                if (alert_msg !== "")
+                    utilities.bad_val_gu_ev.classList.remove("d-none")
+                utilities.bad_val_gu_ev.innerHTML = alert_msg;
+            });
         },
     }
 })()
